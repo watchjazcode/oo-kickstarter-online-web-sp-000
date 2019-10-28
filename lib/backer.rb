708b1e3cd8b2aa17592d60999941d16f5787fa1e
Jazmin M <jazminmerckel@gmail.com>
2019-10-28 00:16:08 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,9 +1,10 @@
 class Backer
   
+  attr_accessor :name
   attr_reader :backed_projects
   
   def initialize(name)
-    @backed_projects = backed_projects
+    @backed_projects = []
     @name = name 
   end
   
